82f403c6ad858e235cb57d0b4c536174d4bc8430
kaller01 <martin@kallers.se>
2023-10-03 22:42:46 +0200

    Production is myt testing enviorment

--- api/posts/controller.js
+++ api/posts/controller.js
@@ -4,33 +4,36 @@ const Location = require("../../models/Location")
 const ObjectId = require("mongoose").Types.ObjectId;
 
 const findPhotoFilenames = async (content) => {
-  const filenames = content.match(/!PHOTO\((.*)\)/g).map(photo => photo.match(/!PHOTO\((.*)\)/)[1])
-  if(!filenames){
+  try {
+    const filenames = content.match(/!PHOTO\((.*)\)/g).map(photo => photo.match(/!PHOTO\((.*)\)/)[1])
+    return (await Photo.find({ filename: { $in: filenames } })).map(x => x._id);
+  } catch (error) {
     return Promise.resolve([])
   }
-  return (await Photo.find({ filename: { $in: filenames } })).map(x => x._id);
 }
 
 const findLocations = async (content) => {
-  const timelines = content.match(/!TIMELINE\{(.*)\}/g).map(x => x.match(/!TIMELINE\{(.*)\}/)[1]);
-  if(!timelines) {
+  try {
+    const timelines = content.match(/!TIMELINE\{(.*)\}/g).map(x => x.match(/!TIMELINE\{(.*)\}/)[1]);
+    const locations = timelines.map(x => x.split(",")[0]);
+    return (await Location.find({ link: { $in: locations } })).map(x => x._id)
+  } catch (error) {
     return Promise.resolve([])
   }
-  const locations = timelines.map(x => x.split(",")[0]);
-  return (await Location.find({ link: { $in: locations } })).map(x => x._id)
+
 }
 
 module.exports = {
   all: async (req, res) => {
-    const posts = await Post.find().populate("cover","paths").sort("-date");
+    const posts = await Post.find().populate("cover", "paths").sort("-date");
     res.json(posts);
   },
   find: async (req, res) => {
     let post;
     if (ObjectId.isValid(req.params.id))
-      post = await Post.findById(req.params.id).populate("cover","paths").populate("photos","paths");
+      post = await Post.findById(req.params.id).populate("cover", "paths").populate("photos", "paths");
     else
-      post = await Post.findOne({ link: req.params.id }).populate("cover","paths").populate("photos","paths");
+      post = await Post.findOne({ link: req.params.id }).populate("cover", "paths").populate("photos", "paths");
     res.json(post);
   },
   add: async (req, res) => {
